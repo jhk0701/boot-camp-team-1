5799e3bfa15efdaeadca103b90e903144d0b3b28
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-09-27 00:09:58 +0900

    Feat: Unit

--- project-TextRPG/Program.cs
+++ project-TextRPG/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace project_TextRPG
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
---
+++ project-TextRPG/Unit.cs
@@ -0,0 +1,19 @@
+﻿namespace project_TextRPG
+{
+    class Unit
+    {
+        public String Name { get; set; }
+        
+        public int Level { get; set; }
+        public int Exp { get; set; }
+
+        public float Attack { get; set; }
+        public float Defense { get; set; }
+
+        public float MaxHealth { get; set; }
+        public float Health { get; set; }
+
+        public float MaxMana { get; set; }
+        public float Mana { get; set; }
+    }
+}
