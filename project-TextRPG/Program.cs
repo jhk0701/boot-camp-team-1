d36bf009cc1a2432272bb6d993f978845ce42dba
PRODOINGER <wjddydghk668@naver.com>
2024-09-30 20:00:45 +0900

    init

--- project-TextRPG/Program.cs
+++ project-TextRPG/Program.cs
@@ -1,7 +1,11 @@
-﻿namespace project_TextRPG
+﻿using System.Text.Json;
+
+namespace project_TextRPG
 {
     class Program
     {
+        static string saveFilePath = "saveData.json";
+
         const string COMMON_NAME = "악덕 기업";
 
         static void Main(string[] args)
@@ -15,5 +19,12 @@
             IScene gameScene = new GameScene(COMMON_NAME);
             gameScene.Start(player);
         }
+        static void SaveGame()
+        {
+            string jsonString = JsonSerializer.Serialize(playerCharacter);
+            File.WriteAllText(saveFilePath, jsonString);
+            Console.WriteLine("게임이 저장되었습니다.");
+        }
+
     }
 }
