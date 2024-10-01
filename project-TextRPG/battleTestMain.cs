8a430b046f83f4f193c00191230db00f4e844e3e
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2024-10-01 17:47:44 +0900

    Fix: start scene

--- project-TextRPG/battleTestMain.cs
+++ project-TextRPG/battleTestMain.cs
@@ -11,17 +11,17 @@ namespace project_TextRPG
     {
         const string COMMON_NAME = "악덕 기업";
 
-        static void Main(string[] args)
-        {
-            Character player = null;
-            IScene startScene = new StartScene(COMMON_NAME, true);
-            startScene.Start(player);
-            player = startScene.End();
-            player.Gold += 10000;
+        //static void Main(string[] args)
+        //{
+        //    Character player = null;
+        //    IScene startScene = new StartScene(COMMON_NAME, true);
+        //    startScene.Start(player);
+        //    player = startScene.End();
+        //    player.Gold += 10000;
 
-            // 게임 시작
-            IScene gameScene = new GameScene(COMMON_NAME);
-            gameScene.Start(player);
-        }
+        //    // 게임 시작
+        //    IScene gameScene = new GameScene(COMMON_NAME);
+        //    gameScene.Start(player);
+        //}
     }
 }
